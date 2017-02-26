85f82d4f52019f83566639692591c471b6c69476
Ussama Naal <usa.naal@gmail.com>
2017-02-26 00:41:43 -0500

    More fixes

--- README.md
+++ README.md
@@ -12,29 +12,13 @@ output:
 This repository contains all required data and scripts to fullfil the assignment plus the complete tidy data.  
 Files:  
 * README.md: this file.  
-* tidy_data.R: is the code book "R Script"" that transforms and tidy the data then generate results.  
-* measurements-tidy.csv: the completed tidy data of measurmentes. the file contains the following variables:  
-  + tBodyAcc.mean.X  
-  + tBodyAcc.mean.Y  
-  + tBodyAcc.mean.Z  
-  + tBodyAcc.std.X  
-  + tBodyAcc.std.Y  
-  + tBodyAcc.std.Z  
-  + Subject  
-  + Activity  
-* averages-tidy.csv: averages of measurements after being grouped by Activity and Subject. the file contains the following variables:  
-  + Activity  
-  + Subject  
-  + mean(tBodyAcc.mean.X)  
-  + mean(tBodyAcc.mean.Y)  
-  + mean(tBodyAcc.mean.Z)  
-  + mean(tBodyAcc.std.X)  
-  + mean(tBodyAcc.std.Y)  
-  + mean(tBodyAcc.std.Z)  
+* run_analysis.R: is the code book "R Script"" that transforms and tidy the data then generate results.  
+* averages-tidy.txt: averages of measurements after being grouped by Activity and Subject.  
+* CodeBook.md: contains additional details on the data found in averages-tidy.txt  
 
 To generate the results yourself you would need to:  
 1. Clone the repository to some local folder on your computer.  
 2. From R\RStudio environment make the folder the current active directory by executing the "setwd" command.  
-3. Execute the R script "tidy_data.R"  
+3. Execute the R script "run_analysis.R"  
 
-For the exact steps on how the results are generated please review the code and the comments in "tidy_data.R"  
+For the exact steps on how the results are generated please review the code and the comments in "run_analysis.R"  
--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,21 @@
 library(dplyr)
+library(stringr)
 
 # 1. Merges the training and the test sets to create one data set.
 
-# load measurments
+## load measurments
 train_x <- scan(file = "train\\X_train.txt", what = numeric())
 train_x <- matrix(train_x, ncol = 561, byrow = TRUE)
 test_x <- scan(file = "test\\X_test.txt", what = numeric())
 test_x <- matrix(test_x, ncol = 561, byrow = TRUE)
 measurements <- rbind(train_x, test_x)  # merge the two matrices row-wise
 
-# load activities
+## load activities
 train_y <- scan(file = "train\\Y_train.txt", what = numeric())
 test_y <- scan(file = "test\\Y_test.txt", what = numeric())
 activities <- c(train_y, test_y)  #train_y and test_y are both vectors
 
-# load subjects
+## load subjects
 train_subject <- scan(file = "train\\subject_train.txt", what = numeric())
 test_subject <- scan(file = "test\\subject_test.txt", what = numeric())
 subjects <- c(train_subject, test_subject)  #train_subject and test_subject are both vectors
@@ -23,38 +24,27 @@ total <- cbind(measurements, subjects, activities)  # merge the three tables col
 total.df <- as.data.frame(total)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-
-# mean and std are the first 6 columns, also pick subjects and activity columns
-new.df <- total.df %>% select(1:6, subjects, activities)
+measurementsLabels <- read.table(file="features.txt", header = FALSE, sep = " ", quote = "", stringsAsFactors = FALSE, col.names = c("featureIndex", "featureLabel"))
+measurmentsWithMeanStd <- str_match(measurementsLabels$featureLabel, "mean|std")
+indicesofMeasurmentsWithMeanStd <- which(!is.na(measurmentsWithMeanStd))
+new.df <- total.df %>% select(indicesofMeasurmentsWithMeanStd, subjects, activities) # Selecting only measurments that has mean or std in them plus subject and activity
 
 # 3. Uses descriptive activity names to name the activities in the data set
-
 activityLabels <- read.table(file="activity_labels.txt", header = FALSE, sep = " ", quote = "", col.names = c("activities", "labels"))
-
 merged.df <- merge(x = new.df, y = activityLabels)  # merge will happen by activities columns
 merged.df$activities <- NULL # drop activities column as it was only used for merging the two datasets.
 str(merged.df)
 
 # 4. Appropriately labels the data set with descriptive variable names.
-
-colnames(merged.df) <- c("tBodyAcc.mean.X", "tBodyAcc.mean.Y", "tBodyAcc.mean.Z", "tBodyAcc.std.X", "tBodyAcc.std.Y", "tBodyAcc.std.Z", "Subject", "Activity")
+namesofMeasurmentsWithMeanStd <- measurementsLabels$featureLabel[indicesofMeasurmentsWithMeanStd]
+colnames(merged.df) <- c(namesofMeasurmentsWithMeanStd, "Subject", "Activity")
 str(merged.df)
-
-write.csv(x = merged.df, file = "measurements-tidy.csv", row.names = F)
 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 average.df <- 
   merged.df %>% 
   group_by(Activity, Subject) %>%
-  summarise(
-    mean(tBodyAcc.mean.X), 
-    mean(tBodyAcc.mean.Y),
-    mean(tBodyAcc.mean.Z),
-    mean(tBodyAcc.std.X),
-    mean(tBodyAcc.std.Y),
-    mean(tBodyAcc.std.Z))
+  summarise_each(funs(mean))
 
-head(average.df) # display results
-
-write.csv(x = average.df, file = "averages-tidy.csv", row.names = F)
+write.table(x = average.df, file = "averages-tidy.txt", row.names = F) # Store results
